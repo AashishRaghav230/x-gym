790c2855cf7e627eaf004f3c72c8cf29a2596422
AashishRaghav230 <rajputashish8905@gmail.com>
2025-10-10 17:52:07 +0600

    Updated homepage responsiveness and fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,40 +13,23 @@ function Footer() {
     return ( 
         <>
         <div>
-       <div className=" bg-neutral-900 h-32 " style={{backgroundImage:`url(${free})`, backgroundSize:"cover",backgroundPosition:"center"}}>
-        <div className="flex gap-2 text-6xl font-extrabold ml-32 relative top-5">
+       <div className=" bg-neutral-900 md:h-32 h-[400px] " style={{backgroundImage:`url(${free})`, backgroundSize:"cover",backgroundPosition:"center"}}>
+        <div className="md:flex gap-2 md:text-6xl text-5xl font-extrabold md:ml-32 ml-36 relative top-5">
             <h1 className="text-white">FREE</h1>
-            <h1 className="text-black" style={{backgroundColor:"yellow"}}>FITNESS</h1>
-            <h1 className="text-white">TIPS</h1>
+            <h1 className="text-black md:w-[244px] w-[200px] mt-3 md:mt-0 relative right-12 md:right-0" style={{backgroundColor:"yellow"}}>FITNESS</h1>
+            <h1 className="text-white mt-2 md:mt-0">TIPS</h1>
         </div>
-        <div className="flex">
-            <h1 className="text-xl ml-32 text-white relative top-5">Get a weekly dose of fitness directly into your mailbox.</h1>
-            <input type="text" placeholder=" Enter your email " className="ml-40 h-10  relative bottom-2 w-96 px-5" />
-            <button className=" text-black font-semibold  px-5 relative  bottom-2" style={{backgroundColor:"yellow", borderColor:"yellow"}}>SUBSCRIBE</button>
+        <div className="md:flex">
+            <h1 className="text-xl md:ml-32 ml-10 text-white relative md:top-5 top-10">Get a weekly dose of fitness directly <span className="md:ml-0 ml-20">into your mailbox.</span></h1>
+            <input type="text" placeholder=" Enter your email " className="md:ml-40 ml-36 h-10  relative md:bottom-2 md:w-96 w-56 px-5 mt-20 md:mt-0" />
+            <button className=" text-black font-semibold  px-5 py-2 relative  md:bottom-2 bottom-10 ml-4 md:ml-0" style={{backgroundColor:"yellow", borderColor:"yellow"}}>SUBSCRIBE</button>
         </div>
        </div>
-       <div className="bg-black h-96">
-        <div className="flex gap-40 relative top-16">
-       <div className="flex ml-20">
-            <h1 className=" first text-4xl font-semibold mt-5 p-3 pl-4 pr-4 text-black" style={{backgroundColor:"yellow"}}>X</h1>
-            <h1 className="ml-2 text-4xl font-semibold mt-8 text-white">G Y M</h1>
-        </div>
-        <div>
-            <h1 className="text-xl mt-10 italic font-semibold ml-20" style={{color:"yellow"}}>CONTACT US</h1>
-            <span class="absolute w-16 h-0.5 bg-white ml-20"></span>
-        </div>
-        <div>
-            <h1 className="text-xl mt-10 italic font-semibold ml-32" style={{color:"yellow"}}>SITEMAP</h1>
-            <span class="absolute w-10 h-0.5 bg-white ml-32"></span>
-        </div>
-        <div>
-            <h1 className="text-xl mt-10 italic font-semibold mr-10" style={{color:"yellow"}}>FOLLOW US</h1>
-            <span class="absolute w-16 h-0.5 bg-white"></span>
-        </div>   
-       </div>
-       <div className="flex gap-16 mt-28">
-        <div>
-            <h1 className="text-white ml-20">Push through the pain, rise with the fire within. <br/>      
+       <div className="bg-black md:h-[385px] h-[1420px] ">
+        <div className="md:flex relative top-20">
+       <div className="md:ml-20">
+            <h1 className=" text-4xl font-semibold md:mt-5 md:ml-0 ml-28 text-white"><span className="px-4 py-2 text-black" style={{backgroundColor:"yellow"}}>X</span> G Y M</h1>
+             <h1 className="text-white mt-16 md:ml-0 ml-8">Push through the pain, rise with the fire within. <br/>      
                 Train like a beast, grow stronger from within.   <br/>     
                 Every drop of sweat is a step toward the goal.  <br/>      
                 Your mind is the muscle that drives your soul. <br/>       
@@ -56,28 +39,33 @@ function Footer() {
                 The battle is daily, the end is not set.   <br/>           
                 Break the limits, rewrite your fate each day.  <br/>       
                 You are the storm—train hard, don't fade away.       
-                 </h1>
+                 </h1> 
         </div>
-        <div>
-            <div className="flex">
+        <div className="md:ml-16 mt-8 ">
+            <h1 className="text-xl  italic font-semibold  md:ml-1 ml-32 md:mt-0 mt-16" style={{color:"yellow"}}>CONTACT US</h1>
+            <span class="absolute w-16 h-0.5 bg-white  md:ml-1 ml-32"></span>
+            <div className="flex mt-16 md:ml-0 ml-9">
         <FaLocationDot className="mt-1 text-2xl " style={{color:"yellow"}}/>
         <h1 className="text-white ml-5">House no‑18, ram Colony, Sector 40,<br/> 
             Gurugram, Haryana 122001, India</h1>
             </div>
-            <span class="absolute  mt-5 h-0.5 ml-1 " style={{backgroundColor:"yellow", width:"330px"}}></span>
-            <div className="flex mt-9">
+            <span class="absolute  mt-5 h-0.5  md:ml-1 ml-7 " style={{backgroundColor:"yellow", width:"330px"}}></span>
+            <div className="flex mt-9 md:ml-0 ml-9">
             <FaPhoneVolume className="mt-1 text-xl " style={{color:"yellow"}}/>
         <h1 className="text-white ml-5 ">+1(603)535-4592<br/>+1(603)535-4556 </h1>
             </div>
-            <span class="absolute mt-4 h-0.5 ml-1 " style={{backgroundColor:"yellow", width:"330px"}}></span>
-            <div className="flex mt-9">
+            <span class="absolute mt-4 h-0.5 md:ml-1 ml-7 " style={{backgroundColor:"yellow", width:"330px"}}></span>
+            <div className="flex mt-9 md:ml-0 ml-9">
             <MdEmail className="mt-1 text-2xl " style={{color:"yellow"}}/>
         <h1 className="text-white ml-5 ">Contact@xgym.com <br/>www.xgym.com </h1>
             </div>
-            <span class="absolute  mt-4 h-0.5 ml-1" style={{backgroundColor:"yellow", width:"330px"}}></span>
+            <span class="absolute  mt-4 h-0.5 md:ml-1 ml-7" style={{backgroundColor:"yellow", width:"330px"}}></span>
         </div>
-        <div className="text-white ml-12 text-lg">
-        <ul className="ml-1">
+        <div className="md:ml-32 ml-36">
+            <h1 className="text-xl md:mt-10 mt-16 italic font-semibold " style={{color:"yellow"}}>SITEMAP</h1>
+            <span class="absolute w-10 h-0.5 bg-white "></span>
+            <div className="text-white  text-lg mt-16">
+            <ul className="">
             <li className="mt-1">Home</li>
             <li className="mt-1">About Us</li>
             <li className="mt-1">Classes</li>
@@ -85,16 +73,22 @@ function Footer() {
             <li className="mt-1">Contact</li>
         </ul>
        </div>
-       <div className="ml-28">
+        </div>
+        
+        <div className="md:ml-40 ml-36 ">
+            <h1 className="text-xl  italic font-semibold md:mt-10 mt-16" style={{color:"yellow"}}>FOLLOW US</h1>
+            <span class="absolute w-16 h-0.5 bg-white"></span>
+            <div className="mt-16 ml-3">
         <button className="border p-2 rounded-lg ml-1" style={{backgroundColor:"yellow",borderColor:"yellow" }}><FaFacebook className="text-black text-3xl" /></button><br/>    
         <button className="border p-2 rounded-lg mt-3  ml-1" style={{backgroundColor:"yellow",borderColor:"yellow" }}><FaInstagram className="text-black text-3xl" /></button><br/>    
         <button className="border p-2 rounded-lg mt-3  ml-1" style={{backgroundColor:"yellow",borderColor:"yellow" }}><FaTwitter className="text-black text-3xl" /></button><br/>    
         <button className="border p-2 rounded-lg mt-3  ml-1" style={{backgroundColor:"yellow",borderColor:"yellow" }}><FaLinkedin  className="text-black text-3xl" /></button>
        </div>
+        </div>   
+        </div>
        </div>
        </div>
-       </div>
-       <span class="absolute mt-36  ml-24 bg-white opacity-15  " style={{ width:"1160px", height:"1px"}}></span>
+       <span class="absolute mt-36  md:ml-24 bg-white opacity-15  " style={{ width:"1160px", height:"1px"}}></span>
        <div className="bg-black text-white text-center h-60 ">
         <h1 className="relative top-44">X Gym &copy; 2025 Paradise Fitness & Health Pvt. Ltd. All Rights Reserved.</h1>
        </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -33,110 +33,122 @@ function Home() {
         <div className="">
            <div className="back ">
             <div className="relative md:top-52 top-56">
-            <div className="flex md:ml-96 md:text-7xl ml-11 text-3xl ">
-           <h1 className="text-white md:ml-12  ">TRAIN </h1>
+            <div className="flex md:ml-96 md:text-7xl ml-20 text-3xl ">
+           <h1 className="text-white md:ml-12 ml-2 ">TRAIN </h1>
            <h1 className="insane md:ml-10 ml-4" >INSANE</h1>
            </div>
-           <div className="flex items-center md:max-w-3xl gap-3 md:ml-72 ml-6">
+           <div className="flex items-center md:max-w-3xl gap-3 md:ml-72 ml-16">
            <div className="line1 md:flex-1 md:h-10  md:mt-4 h-4 w-24 mt-2 "></div>
            <span className=" text-white md:mt-4 md:text-4xl text-xl mt-2">OR</span>
            <div className="line2 md:flex-1 md:h-10 md:mt-4 h-4 w-24 mt-2"></div>
            </div>
-           <div className="flex md:ml-80 md:text-7xl md:mt-4 text-3xl ml-5 mt-2">
-           <h1 className="text-white md:ml-12">REMAIN THE </h1>
+           <div className="flex md:ml-80 md:text-7xl md:mt-4 text-3xl ml-12 mt-2">
+           <h1 className="text-white md:ml-12 ml-3">REMAIN THE </h1>
            <h1 className="same md:ml-7 ml-2">SAME</h1>
            </div>
            
            <div className="md:ml-28 md:mt-16 mt-20">
-           <button className="btn1 border md:px-4 md:py-2 px-3 py-1 md:ml-96 font-semibold relative left-20 ml-2 md:left-0 " onClick={() => openModal("free")}>TRY US FREE</button>
-           <button className="btn2 md:ml-28 border  md:px-5 md:py-2 px-4 py-1 font-semibold relative top-20 md:top-0 md:right-0 right-9 " onClick={() => openModal("join")}>JOIN NOW</button>
+           <button className="btn1 border px-4 py-2  md:ml-96 font-semibold relative left-20 ml-11 md:left-0 " onClick={() => openModal("free")}>TRY US FREE</button>
+           <button className="btn2 md:ml-28 border  px-5 py-2  font-semibold relative top-20 md:top-0 md:right-0 right-11 " onClick={() => openModal("join")}>JOIN NOW</button>
            </div>
            <FormModal isOpen={modalOpen} onClose={closeModal} type={modalType} />
            </div>
         </div>
-        <div className=" h-screen bg-neutral-900 text-white text-center ">
+        <div className="fitness md:h-screen h-[1650px] bg-neutral-900 text-white text-center ">
             <div data-aos="fade-up">
          <h1 className="relative top-12 text-lg tracking-widest ">EXPERIENCED & INTERNATIONALLY CERTIFIED PERSONAL TRAINERS</h1>
          <h1 className=" mt-16  italic h-10 text-5xl" style={{color:"yellow"}}>THE BEST FITNESS EXPERIENCE</h1>
-         <h1 className="mt-8">We're committed to bringing you the best workout experience.</h1>
-        <div className="flex ml-16  mt-16 gap-5">
-         <img className="h-60 w-72 rounded-lg" src={bodycomp} />
-         <img className="h-60 w-72 rounded-lg" src={sevenday} />
-         <img className="h-60 w-72 rounded-lg" src={parking} />
-         <img className="h-60 w-72 rounded-lg" src={clean} />
+         <h1 className="md:mt-8 mt-32">We're committed to bringing you the best workout experience.</h1>
+        <div className="md:flex md:ml-16  mt-16 gap-5 ml-11">
+             <div>
+                 <img className="h-60 w-72 rounded-lg" src={bodycomp} />
+                 <h1 className=" md:mt-9 mt-6 relative md:right-0 right-5">Body Composition Assessment</h1>
+             </div>
+             <div>
+                  <img className="h-60 w-72 rounded-lg md:mt-0 mt-6" src={sevenday} />
+                  <h1 className=" md:mt-9 mt-6 relative md:right-0 right-5">Open 7 Days A Week</h1>
+             </div>
+             <div>
+                  <img className="h-60 w-72 rounded-lg md:mt-0 mt-6" src={parking} />
+                  <h1 className=" md:mt-9 mt-6 relative md:right-0 right-6">Complimentary Valet Parking</h1>
+             </div>
+             <div>
+                  <img className="h-60 w-72 rounded-lg md:mt-0 mt-6" src={clean} />
+                  <h1 className=" md:mt-9 mt-6 relative md:right-0 right-5">Clean, Comfortable and Safe</h1>
+             </div>
         </div>
-        <div className="flex ml-20 mt-9">
-         <h1 className="ml-4">Body Composition Assessment</h1>
-         <h1 className="ml-32">Open 7 Days A Week</h1>
-         <h1 className="ml-32">Complimentary Valet Parking</h1>
-         <h1 className="ml-28">Clean, Comfortable and Safe</h1>
-         </div>
         </div>
         </div>
-        <div className="homeimg2 ">
+        <div className="homeimg2 md:h-screen h-[750px]">
             <div data-aos="fade-up">
-         <h1 className="text-4xl text-white font-bold relative top-40 ml-12 scale-y-[1.5] font-sans">"During the hard times, it's important to <br />
+         <h1 className="md:text-4xl text-3xl text-white font-bold relative top-40 md:ml-12 ml-4 scale-y-[1.5] font-sans">"During the hard times, it's important to <br />
              focus on the things you can change in that <br />
              moment instead of what you should have or <br />
              could have done differently."</h1>
-             <div className="flex mt-80 ml-12 gap-2 text-2xl font-semibold font-sans">
+             <div className="flex mt-80 md:ml-12 ml-4 gap-2 md:text-2xl text-xl font-semibold font-sans">
              <h1 className="text-white mt-4">- Annie Thorisdottir,</h1>
              <h1 className="text-white mt-4" style={{color:"yellow"}}>X GYM Athlete</h1>
              </div>
              </div>
         </div>
-        <div className="h-screen bg-neutral-900">
-            <div data-aos="fade-up" className="flex ">
-            <div className=" mt-16 relative left-24">
-            <h1 className="text-white">WHO WE ARE</h1>
-            <h1 className="text-4xl font-bold text-white mt-3" style={{color:""}} >Building Fitness is Building Your<br/>
+        <div className="md:h-[670px] h-[1550px] bg-neutral-900">
+            <div data-aos="fade-up" className="md:flex ">
+            <div className=" md:mt-16 relative md:left-24 md:top-0 top-12">
+            <h1 className="text-white ml-5 md:ml-0">WHO WE ARE</h1>
+            <h1 className="md:text-4xl text-3xl font-bold text-white mt-3 md:ml-0 ml-5" style={{color:""}} >Building Fitness is Building Your<br/>
                 Body and Confidence
             </h1>
-            <h1 className="text-white mt-3">we believe that fitness is not just about building muscles, it’s about
+            <h1 className="text-white mt-3 ml-5 md:ml-0">we believe that fitness is not just about building muscles, it’s about
                  building <br/>confidence , discipline , and a healthier lifestyle. With 
                  certified trainers , modern equipment,<br/> and result-oriented programs
                  — we help you transform your body and mindset.</h1>
                  <div className="flex mt-5">
-                 <MdArrowOutward className="mt-1 text-lg" style={{color:"yellow"}}/><h1 className="text-white font-bold">Personal Trainer</h1>
-                 <MdArrowOutward className="ml-48 mt-1 text-lg" style={{color:"yellow"}}/><h1 className="text-white font-bold">Cardio Programs</h1>
+                 <MdArrowOutward className="mt-1 text-lg ml-5 md:ml-0" style={{color:"yellow"}}/><h1 className="text-white font-bold">Personal Trainer</h1>
+                 <MdArrowOutward className="md:ml-48 ml-11 mt-1 text-lg" style={{color:"yellow"}}/><h1 className="text-white font-bold">Cardio Programs</h1>
                  </div>
                  <div className="flex mt-1">
-                    <h1 className="text-white">Get 1-on-1 attention from certified fitness<br/> coaches to help you train smarter.</h1>
-                    <h1 className="ml-10 text-white">Improve your heart health and stamina with<br/> our tailored cardio routines.</h1>
+                    <h1 className="text-white ml-5 md:ml-0">Get 1-on-1 attention from certified fitness<br/> coaches to help you train smarter.</h1>
+                    <h1 className="md:ml-10 ml-5 text-white">Improve your heart health and stamina with<br/> our tailored cardio routines.</h1>
                  </div>
-                 <div className="flex gap-1 mt-4">
-                 <MdArrowOutward className="mt-1 text-lg" style={{color:"yellow"}}/><h1 className="text-white font-bold ">Quality Equipments</h1>
-                 <MdArrowOutward className="mt-1 ml-40 text-lg" style={{color:"yellow"}}/><h1 className="text-white font-bold">Healthy Nutrition</h1>
+                 <div className="flex  mt-4">
+                 <MdArrowOutward className="mt-1 text-lg ml-5 md:ml-0" style={{color:"yellow"}}/><h1 className="text-white font-bold ">Quality Equipments</h1>
+                 <MdArrowOutward className="mt-1 md:ml-40 relative left-1 ml-4 text-lg" style={{color:"yellow"}}/><h1 className="text-white font-bold relative left-1">Healthy Nutrition</h1>
                  </div>
                  <div className="flex mt-1">
-                    <h1 className="text-white">Train with state-of-the-art fitness machine<br/> for maximum performance.</h1>
-                    <h1 className="ml-10 text-white">Get expert tips and plan on healthy eating<br/> for faster results.</h1>
+                    <h1 className="text-white ml-5 md:ml-0">Train with state-of-the-art fitness machine<br/> for maximum performance.</h1>
+                    <h1 className="md:ml-10 ml-5 text-white">Get expert tips and plan on healthy eating<br/> for faster results.</h1>
                  </div>
             </div>
-            <div className="flex relative right-8 ">
+            <div className="flex relative md:right-8 right-52 ">
                 <img src={women} className=" h-44 relative left-48 w-56 mt-52  " />
                 <img src={men} className="mt-20 ml-2" style={{height:"360px", width:"400px"}} />
             </div>
             </div>
-            <div className="text1 flex text-black gap-60 ml-24 mt-20" >
-                <h1 className="px-3 text-4xl font-semibold"  style={{backgroundColor:"yellow"}}>1374</h1>
-                <h1 className="px-3 text-4xl font-semibold" style={{backgroundColor:"yellow"}}>356</h1>
-                <h1 className="px-3 text-4xl font-semibold" style={{backgroundColor:"yellow"}}>57k</h1>
-                <h1 className="px-3 text-4xl font-semibold" style={{backgroundColor:"yellow"}}>17+</h1>
+            <div className="md:flex text-black md:ml-24 ml-32  mt-20" >
+                <div>
+                <h1 className=" px-3 text-4xl font-semibold " style={{backgroundColor:"yellow",width:"110px",fontFamily:" 'PT Sans', sans-serif"}} >1374</h1>
+                <h1 className="mt-1  text-white text-lg ">Happy Clients</h1>
+                </div>
+                <div>
+                <h1 className="px-3 text-4xl font-semibold mt-10 md:mt-0 md:ml-60 ml-3"style={{backgroundColor:"yellow",width:"86px",fontFamily:" 'PT Sans', sans-serif"}} >356</h1>
+                <h1 className="mt-1 text-white text-lg md:ml-56  relative left-1">Strong Bodies</h1>
+                </div>
+                <div>
+                <h1 className="px-3 text-4xl font-semibold mt-10 md:mt-0 md:ml-60 ml-3"style={{backgroundColor:"yellow",width:"85px",fontFamily:" 'PT Sans', sans-serif"}} >57k</h1>
+                <h1 className="mt-1 text-white text-lg md:ml-56  relative left-2">IG Followers</h1>
+                </div>
+                <div>
+                <h1 className="px-3 text-4xl font-semibold mt-10 md:mt-0 md:ml-60 ml-3" style={{backgroundColor:"yellow",width:"85px",fontFamily:" 'PT Sans', sans-serif"}}>17+</h1>
+                <h1 className="mt-1 text-white text-lg md:ml-52 relative md:left-2 right-3 md:right-0">Years Experience</h1>
+                </div>
             </div>
         </div>
-        <div className="flex bg-neutral-900 text-white text-lg gap-52 h-16 w-full">
-            <h1 className="ml-24 relative bottom-6">Happy Clients</h1>
-            <h1 className="ml-4 relative bottom-6">Strong Bodies</h1>
-            <h1 className="ml-3 relative bottom-6">IG Followers</h1>
-            <h1 className="ml- relative bottom-6">Years Experience</h1>
         </div>
-        </div>
-        <div className="pricing2 bg-black" style={{height:"870px"}}>
+        <div className="pricing2 bg-black md:h-[870px] h-[3100px]">
             <div data-aos="fade-up">
-            <h1 className="text-white text-5xl font-bold text-center relative top-20">CHOOSE YOUR <span style={{backgroundColor:"yellow",color:"black"}}>PRICING</span> PLAN</h1>
-            <div className="ml-2 mt-2 flex gap-10">
-                <div className="pricing text-white px-5 border border-white  ml-16  mt-40">
+            <h1 className="text-white md:text-5xl text-4xl font-bold text-center relative top-20">CHOOSE YOUR <span style={{backgroundColor:"yellow",color:"black"}}>PRICING</span> PLAN</h1>
+            <div className="md:ml-2 mt-2 md:flex gap-10">
+                <div className="pricing text-white px-5 border border-white  md:ml-16 ml-12 mt-40">
                     <h1 className="text-5xl font-bold text-center mt-16">$10</h1>
 						<p className="text-center mt-3 text-lg">/month</p>
 						<ul className=" mt-16">
@@ -148,7 +160,7 @@ function Home() {
 						</ul>
                         <button className="choose1  border  font-semibold mt-52 ml-7 text-lg px-8 py-4">Choose Plan</button>
             </div> 
-            <div className="pricing text-white px-5 border border-white  mt-40" >
+            <div className="pricing text-white px-5 border border-white  mt-40 ml-12 md:ml-0" >
                     <h1 className="text-5xl font-bold text-center mt-16">$25</h1>
 						<p className="text-center mt-3 text-lg">/month</p>
 						<ul className=" mt-16">
@@ -160,7 +172,7 @@ function Home() {
 						</ul>
                         <button className="choose2 border  font-semibold  ml-7 text-lg px-8 py-4">Choose Plan</button>
             </div>          
-            <div className="pricing text-white px-5 border border-white  mt-40" >
+            <div className="pricing text-white px-5 border border-white  mt-40 ml-12 md:ml-0" >
                     <h1 className="text-5xl font-bold text-center mt-16">$30</h1>
 						<p className="text-center mt-3 text-lg">/month</p>
 						<ul className=" mt-16">
@@ -172,7 +184,7 @@ function Home() {
 						</ul>
                         <button className="choose3 hover:bg-black border font-semibold  ml-7 text-lg px-8 py-4">Choose Plan</button>
             </div>          
-            <div className="pricing text-white px-5 border border-white  mt-40">
+            <div className="pricing text-white px-5 border border-white  mt-40 ml-12 md:ml-0">
                     <h1 className="text-5xl font-bold text-center mt-16">$50</h1>
 						<p className="text-center mt-3 text-lg">/month</p>
 						<ul className=" mt-16">
