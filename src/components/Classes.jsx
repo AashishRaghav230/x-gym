0652e8c4a3b766ad92d446119b5d263ebc11b3c1
AashishRaghav230 <rajputashish8905@gmail.com>
2025-10-15 18:46:04 +0530

    Updated classes and trainers page

--- src/components/Classes.jsx
+++ src/components/Classes.jsx
@@ -14,99 +14,91 @@ import "aos/dist/aos.css";
 
 function Classes() {
   useEffect(() => {
-    AOS.init({ duration: 1000 }); // एनिमेशन 1 सेकंड का
+    AOS.init({ duration: 1000 }); 
   }, []);
     return (
         <>
     <div className="w-screen overflow-x-hidden m-0 p-0" >
          <div className="classesimg">
-              <div className="relative top-52 ml-96">
-              <h1 className="cont2 text-white text-8xl ml-40 ">CLASSES</h1>
+              <div className="relative top-52 md:ml-96 ml-2">
+              <h1 className="cont2 text-white md:text-8xl text-7xl md:ml-40 ml-24 ">CLASSES</h1>
               </div>
-              <div className="mt-52 ml-52 flex gap-1">
-              <IoHomeSharp className="text-white relative left-96 text-lg mt-1 " /><h1 style={{}} className=" text-white font-semibold ml-96 text-lg " >Home ></h1>
-              <h1 className=" text-white text-lg" style={{color:"yellow"}}>Classes</h1> 
+              <div className="mt-52 md:ml-52 flex gap-1 ml-28 ">
+              <IoHomeSharp className="text-white relative md:left-96 text-lg mt-1 left-2 " /><h1 style={{}} className=" text-white font-semibold md:ml-96 text-lg ml-2" >Home ></h1>
+              <h1 className=" text-white text-lg " style={{color:"yellow"}}>Classes</h1> 
               </div>
          </div>
-         <div className="bg-neutral-900 " style={{height:"700px"}}>
+         <div className="bg-neutral-900 md:h-[700px] h-[1125px] " >
               <div data-aos="fade-up">
               <h1 className="text-center relative top-12 text-3xl" style={{color:"yellow"}}>X GYM</h1>
-              <h1 className=" text-white text-center relative top-12 mt-1 text-lg italic  ">Take care of your body. It's the only place you have to live.</h1>
+              <h1 className=" text-white text-center relative top-12 mt-1  italic md:text-lg ">Take care of your body. It's the only place you have to live.</h1>
               </div>
-              <div data-aos="fade-up">
-              <div className="text-white mt-20 flex gap-96 ml-64 text-xl">
-                <h1 className="relative left-4 top-1 tracking-wider">GYM FITNESS</h1>
-                <h1 className="relative left-36 top-1 tracking-wide">INDOOR CYCLING</h1>
-              </div>
-              <div className="flex gap-12 ml-12 mt-4">
-                <img src={gymfitness} style={{height:"400px",width:"600px"}}/>
-                <img src={indoorcycling} style={{height:"400px",width:"600px"}}/>
-              </div>
-              <div className="text-white ml-12 mt-3 flex gap-12">
-                <h1>Exercise and rest are the cornerstones of improving your quality of life. It's not always<br/>
+              <div data-aos="fade-up" className="md:flex mt-24 md:ml-12 ml-10 gap-12">
+              <div className=" text-white ">
+                <h1 className="relative left-4  tracking-wider text-xl md:ml-[210px] bottom-3 ml-[72px] ">GYM FITNESS</h1>
+                <img src={gymfitness}  className="md:h-[400px] md:w-[600px] h-[250px] w-[300px] "/>
+                <h1 className="mt-3">Exercise and rest are the cornerstones of improving your quality of life. It's not always<br/>
                    easy but it's always worth it. When you find a fitness routine you enjoy, you'll feel bett<br/>er,
                    look better and be happier in your own skin.</h1>
-                   <h1>Cycling is a great form of exercise as well as a practical way to get around, but it also<br/>
+                   </div>
+              <div className="text-white md:mt-0 mt-20">
+              <h1 className="relative md:left-36  tracking-wide text-xl md:ml-[72px] bottom-3 ml-[70px]">INDOOR CYCLING</h1>
+                <img src={indoorcycling} className="md:h-[400px] md:w-[600px] h-[250px] w-[300px] "/>
+                <h1 className="mt-3">Cycling is a great form of exercise as well as a practical way to get around, but it also<br/>
                      carries the risk of significant injury, especially head injury. involves using a special as<br/>
                      stationary exercise bicycle with a weighted flywheel in a classroom setting.</h1>
               </div>
               </div>
          </div>
-         <div className="bg-neutral-900" style={{height:"600px"}}>
-             <div data-aos="fade-up">
-             <div className="text-white  flex gap-96 ml-64 text-xl">
-                <h1 className="relative left-4 top-12 tracking-wider">POWER YOGA</h1>
-                <h1 className="relative left-36 top-12 tracking-wide">CARDIO FITNESS</h1>
-              </div>
-              <div className="flex gap-12 ml-12 mt-12 relative top-3">
-                <img src={poweryoga} style={{height:"400px",width:"600px"}}/>
-                <img src={cardiofitness} style={{height:"400px",width:"600px"}}/>
-              </div>
-              <div className="text-white ml-12 mt-6 flex gap-12">
-                <h1>Exercise and rest are the cornerstones of improving your quality of life. It's not always<br/>
+         <div className="bg-neutral-900 md:h-[600px] h-[1025px] " >
+             <div data-aos="fade-up" className="md:flex  md:ml-12 ml-10  gap-12 relative top-16">
+             <div className="text-white">
+                <h1 className="relative left-4  tracking-wider text-xl md:ml-[210px] bottom-3 ml-[72px]">POWER YOGA</h1>
+                <img src={poweryoga} className="md:h-[400px] md:w-[600px] h-[250px] w-[300px]"/>
+                <h1 className="mt-3">Exercise and rest are the cornerstones of improving your quality of life. It's not always<br/>
                    easy but it's always worth it. When you find a fitness routine you enjoy, you'll feel bett<br/>er,
                    look better and be happier in your own skin.</h1>
-                <h1>Cycling is a great form of exercise as well as a practical way to get around, but it also<br/>
+                   </div>
+              <div className="text-white md:mt-0 mt-20 ">
+              <h1 className="relative md:left-36  tracking-wide text-xl  ml-[72px] bottom-3">CARDIO FITNESS</h1>
+                <img src={cardiofitness} className="md:h-[400px] md:w-[600px] h-[250px] w-[300px] "/>
+                <h1 className="mt-3">Cycling is a great form of exercise as well as a practical way to get around, but it also<br/>
                     carries the risk of significant injury, especially head injury. involves using a special as<br/>
                     stationary exercise bicycle with a weighted flywheel in a classroom setting.</h1>
               </div>
               </div>
          </div> 
-         <div className="bg-neutral-900" style={{height:"600px"}}>
-             <div data-aos="fade-up">
-             <div className="text-white  flex gap-96 ml-64 text-xl">
-                <h1 className="relative left-1 top-12 tracking-wider">CIRCUIT TRAINING</h1>
-                <h1 className="relative left-36 top-12 tracking-wide">ZUMBA</h1>
-              </div>
-              <div className="flex gap-12 ml-12 mt-12 relative top-3">
-                <img src={circuittraining} style={{height:"400px",width:"600px"}}/>
-                <img src={zumba} style={{height:"400px",width:"600px"}}/>
-              </div>
-              <div className="text-white ml-12 mt-6 flex gap-12">
-                <h1>Exercise and rest are the cornerstones of improving your quality of life. It's not always<br/>
+         <div className="bg-neutral-900 md:h-[600px] h-[1025px] " >
+             <div data-aos="fade-up" className="md:flex  md:ml-12 ml-10  gap-12 relative md:top-16 top-12">
+             <div className="text-white">
+                <h1 className="relative left-4  tracking-wider text-xl md:ml-[190px] ml-[45px] bottom-3">CIRCUIT TRAINING</h1>
+                <img src={circuittraining} className="md:h-[400px] md:w-[600px] h-[250px] w-[300px] "/>
+                <h1 className="mt-3">Exercise and rest are the cornerstones of improving your quality of life. It's not always<br/>
                    easy but it's always worth it. When you find a fitness routine you enjoy, you'll feel bett<br/>er,
                    look better and be happier in your own skin.</h1>
-                <h1>Cycling is a great form of exercise as well as a practical way to get around, but it also<br/>
+                   </div>
+              <div className="text-white md:mt-0 mt-20 ">
+              <h1 className="relative md:left-36  tracking-wide text-xl md:ml-[100px] bottom-3 ml-[115px] ">ZUMBA</h1>
+                <img src={zumba} className="md:h-[400px] md:w-[600px] h-[250px] w-[300px] "/>
+                <h1 className="mt-3">Cycling is a great form of exercise as well as a practical way to get around, but it also<br/>
                     carries the risk of significant injury, especially head injury. involves using a special as<br/>
                     stationary exercise bicycle with a weighted flywheel in a classroom setting.</h1>
               </div>
               </div>
          </div> 
-         <div className="bg-neutral-900" style={{height:"650px"}}>
-             <div data-aos="fade-up">
-             <div className="text-white  flex gap-96 ml-60 text-xl">
-                <h1 className="relative left-1 top-12 tracking-wider">STRENGTH CLASSES</h1>
-                <h1 className="relative left-24 top-12 tracking-wide">OUTDOOR TRAINING</h1>
-              </div>
-              <div className="flex gap-12 ml-12 mt-12 relative top-3">
-                <img src={strengthclasses} style={{height:"400px",width:"600px"}}/>
-                <img src={outdoortraining} style={{height:"400px",width:"600px"}}/>
-              </div>
-              <div className="text-white ml-12 mt-6 flex gap-12">
-                <h1>Exercise and rest are the cornerstones of improving your quality of life. It's not always<br/>
+         <div className="bg-neutral-900 h-[1050px] md:h-[650px] " >
+             <div data-aos="fade-up" className="md:flex  md:ml-12 ml-10  gap-12 relative md:top-16 top-8 ">
+             <div className="text-white">
+                <h1 className="relative left-4  tracking-wider text-xl md:ml-[190px] ml-[40px] bottom-3">STRENGTH CLASSES</h1>
+                <img src={strengthclasses} className="md:h-[400px] md:w-[600px] h-[250px] w-[300px] "/>
+                <h1 className="mt-3">Exercise and rest are the cornerstones of improving your quality of life. It's not always<br/>
                    easy but it's always worth it. When you find a fitness routine you enjoy, you'll feel bett<br/>er,
                    look better and be happier in your own skin.</h1>
-                <h1>Cycling is a great form of exercise as well as a practical way to get around, but it also<br/>
+                   </div>
+              <div className="text-white md:mt-0 mt-20 ">
+              <h1 className="relative md:left-36  tracking-wide text-xl md:ml-[72px] ml-[60px] bottom-3">OUTDOOR TRAINING</h1>
+                <img src={outdoortraining} className="md:h-[400px] md:w-[600px] h-[250px] w-[300px] "/>
+                <h1 className="mt-3">Cycling is a great form of exercise as well as a practical way to get around, but it also<br/>
                     carries the risk of significant injury, especially head injury. involves using a special as<br/>
                     stationary exercise bicycle with a weighted flywheel in a classroom setting.</h1>
               </div>
--- src/components/Trainers.jsx
+++ src/components/Trainers.jsx
@@ -16,49 +16,49 @@ function Trainers() {
         <>
         <div className="w-screen overflow-x-hidden m-0 p-0">
             <div className="trainerimg">
-                <div className="relative top-52 ml-96">
-                <h1 className="cont2 text-white text-8xl ml-40 ">TRAINERS</h1>
+                <div className="relative top-52 md:ml-96 ml-2">
+                <h1 className="cont2 text-white md:text-8xl text-7xl md:ml-40 ml-24 ">TRAINERS</h1>
                 </div>
-                <div className="mt-52 ml-52 flex gap-1">
-                <IoHomeSharp className="text-white relative left-96 text-lg mt-1 " /><h1 style={{}} className=" text-white font-semibold ml-96 text-lg " >Home ></h1>
+                <div className="mt-52 md:ml-56 ml-28 flex gap-1">
+                <IoHomeSharp className="text-white relative md:left-96 left-2 text-lg mt-1 " /><h1 style={{}} className=" text-white font-semibold md:ml-96 ml-2 text-lg " >Home ></h1>
                 <h1 className=" text-white text-lg" style={{color:"yellow"}}>Trainers</h1> 
                 </div>
             </div>
-            <div className="bg-neutral-900" style={{height:"920px"}}>
+            <div className="bg-neutral-900 md:h-[920px] h-[1700px] " >
                 <div data-aos="fade-up">
                 <h1 className="text-center text-3xl relative top-12" style={{color:"yellow",}}>MEET OUR TRAINERS</h1>
-                <h1 className="text-white text-center mt-1 text-lg italic relative top-12">Expert guidance and personalized support to help you succeed.</h1>
+                <h1 className="text-white text-center mt-1 md:text-lg italic relative top-12">Expert guidance and personalized support to help you succeed.</h1>
                 </div>
-                <div data-aos="fade-up" className="flex gap-28">
-                    <div className="mt-32 ml-24">
+                <div data-aos="fade-up" className="md:flex gap-28">
+                    <div className="mt-32 md:ml-24 ml-12 ">
                         <h1 className="text-white text-4xl font-bold">Jason Tran</h1>
                         <span class="absolute w-20 h-1  mt-5" style={{backgroundColor:"yellow"}}></span>
-                        <h1 className="text-white mt-10 text-lg">After eight years in the finance industry, Jason left his desk job<br/>
+                        <h1 className="text-white mt-10 md:text-lg text-xl">After eight years in the finance industry, Jason left his desk job<br/>
                             behind for a career in fitness. Certified by Schwinn, Jason<br/>
                             quickly developed a fiercely loyal following (lovingly known as<br/>
                             Tran Fans) through teaching at various gyms in the city. He's<br/>
                             become well known in NYC by his trademark playlists and<br/>
                             hard-to-find remixes and his intense (but fun!) teaching style.<br/>
                             There's no doubt that after a FHIX or two you will become a<br/> Tran Fan!</h1>
-                        <h1 className="text-white text-lg mt-6"><span className="font-bold">Fun Fact: </span>
+                        <h1 className="text-white md:text-lg text-xl mt-6"><span className="font-bold">Fun Fact: </span>
                             The best piece of advice Jason ever received comes<br/>
                             from Sister Act 2: "If you wanna be somebody, if you wanna go<br/>
                             somewhere, you better wake up and pay attention."</h1>
-                            <h1 className="text-white text-lg mt-6 font-bold">Certifications:</h1>
-                            <h1 className="text-white mt-2 text-lg">Kettlebell Concepts, PROnatal Fitness Pre and Post Natal, TRX <br/>
+                            <h1 className="text-white md:text-lg text-xl mt-6 font-bold">Certifications:</h1>
+                            <h1 className="text-white mt-2 md:text-lg text-xl">Kettlebell Concepts, PROnatal Fitness Pre and Post Natal, TRX <br/>
                              Level 1.</h1>
                              <button className="btn2 mt-8  border  px-4 py-2 font-semibold">BOOK JASON CLASS</button>
                     </div>
                     <div>
-                       <img className="mt-32" src={trainer1} style={{height:"520px",width:"540px"}}/>
+                       <img className="md:mt-32 mt-20 md:h-[520px] md:w-[540px] h-[350px] w-[300px] md:ml-0 ml-10 " src={trainer1}/>
                     </div>
                 </div>
             </div>
-                <div className="flex gap-28 bg-neutral-900" style={{height:"700px"}}>
-                    <div data-aos="fade-up" className="  ml-24">
+                <div className="md:flex gap-28 bg-neutral-900 h-[1400px] md:h-[700px]">
+                    <div data-aos="fade-up" className="  md:ml-24 ml-12">
                         <h1 className="text-white text-4xl font-bold">Melody Scharff</h1>
                         <span class="absolute w-20 h-1  mt-5" style={{backgroundColor:"yellow"}}></span>
-                        <h1 className="text-white mt-10 text-lg">
+                        <h1 className="text-white mt-10 md:text-lg text-xl ">
                                        Melody grew up dancing and developed a passion for<br/>
                                        performing. After performing for over eighteen years, Melody<br/>
                                        felt her love for dance fading and needed to find new ways to<br/>
@@ -67,47 +67,47 @@ function Trainers() {
                                        she left her 9-to-5 to pursue a career in fitness. Melody's goal<br/>
                                        is to help people feel confident both in the studio and their<br/>
                                        day-to-day lives.</h1>
-                        <h1 className="text-white text-lg mt-6"><span className="font-bold">Fun Fact: </span>
+                        <h1 className="text-white md:text-lg text-xl  mt-6"><span className="font-bold">Fun Fact: </span>
                              Mel would love to train Miley Cyrus one day. "I'm all<br/>
                              about releasing some serious girl power!"</h1>
-                            <h1 className="text-white text-lg mt-6 font-bold">Certifications:</h1>
-                            <h1 className="text-white mt-2 text-lg">NASM Personal Trainer, AFAA Group Fitness Instructor,<br/>
+                            <h1 className="text-white md:text-lg text-xl  mt-6 font-bold">Certifications:</h1>
+                            <h1 className="text-white mt-2 md:text-lg text-xl ">NASM Personal Trainer, AFAA Group Fitness Instructor,<br/>
                             PROnatal Fitness Pre and Post Natal, TRX Level 1</h1>
                              <button className="btn2 mt-8  border  px-4 py-2 font-semibold">BOOK MELODY CLASS</button>
                     </div>
                     <div>
-                       <img data-aos="fade-up" className="" src={trainer2} style={{height:"500px",width:"540px"}}/>
+                       <img data-aos="fade-up" className="md:mt-0 mt-20 md:h-[500px] md:w-[540px] h-[320px] w-[300px] md:ml-0 ml-10" src={trainer2} />
                     </div>
                 </div>
-                <div className="flex gap-28 bg-neutral-900" style={{height:"700px"}}>
-                    <div data-aos="fade-up" className=" ml-24">
+                <div className="md:flex gap-28 bg-neutral-900 h-[1400px] md:h-[700px]">
+                    <div data-aos="fade-up" className=" md:ml-24 ml-12 ">
                         <h1 className="text-white text-4xl font-bold">Lucas Donovan</h1>
                         <span class="absolute w-20 h-1  mt-5" style={{backgroundColor:"yellow"}}></span>
-                        <h1 className="text-white mt-10 text-lg">After eight years in the finance industry, Jason left his desk job<br/>
+                        <h1 className="text-white mt-10 md:text-lg text-xl ">After eight years in the finance industry, Jason left his desk job<br/>
                             behind for a career in fitness. Certified by Schwinn, Lucas<br/>
                             quickly developed a fiercely loyal following (lovingly known as<br/>
                             Donovan Fans) through teaching at various gyms in the city. He's<br/>
                             become well known in NYC by his trademark playlists and<br/>
                             hard-to-find remixes and his intense (but fun!) teaching style.<br/>
                             There's no doubt that after a FHIX or two you will become a<br/> Tran Fan!</h1>
-                            <h1 className="text-white text-lg mt-6"><span className="font-bold">Fun Fact: </span>
+                            <h1 className="text-white  md:text-lg text-xl mt-6"><span className="font-bold">Fun Fact: </span>
                             The best piece of advice Jason ever received comes<br/>
                             from Sister Act 2: "If you wanna be somebody, if you wanna go<br/>
                             somewhere, you better wake up and pay attention."</h1>
-                            <h1 className="text-white text-lg mt-6 font-bold">Certifications:</h1>
-                            <h1 className="text-white mt-2 text-lg">Kettlebell Concepts, PROnatal Fitness Pre and Post Natal, TRX <br/>
+                            <h1 className="text-white  md:text-lg text-xl mt-6 font-bold">Certifications:</h1>
+                            <h1 className="text-white mt-2  md:text-lg text-xl">Kettlebell Concepts, PROnatal Fitness Pre and Post Natal, TRX <br/>
                              Level 1.</h1>
                              <button className="btn2 mt-8  border  px-4 py-2 font-semibold">BOOK LUCAS CLASS</button>
                     </div>
                     <div>
-                       <img data-aos="fade-up" className="" src={trainer3} style={{height:"500px",width:"540px"}}/>
+                       <img data-aos="fade-up" className="md:mt-0 mt-20 md:h-[500px] md:w-[540px] h-[320px] w-[300px] md:ml-0 ml-10" src={trainer3} />
                     </div>
                 </div>
-                <div className="flex gap-28 bg-neutral-900" style={{height:"650px"}}>
-                    <div data-aos="fade-up" className=" ml-24">
-                        <h1 className="text-white text-4xl font-bold">Olivia Parker</h1>
+                <div className="md:flex gap-28 bg-neutral-900 md:h-[700px] h-[1400px] ">
+                    <div data-aos="fade-up" className="md:ml-24 ml-12">
+                        <h1  className="text-white text-4xl font-bold">Olivia Parker</h1>
                         <span class="absolute w-20 h-1  mt-5" style={{backgroundColor:"yellow"}}></span>
-                        <h1 className="text-white mt-10 text-lg">
+                        <h1 className="text-white mt-10 md:text-lg text-xl">
                                        Olivia grew up dancing and developed a passion for<br/>
                                        performing. After performing for over eighteen years, Melody<br/>
                                        felt her love for dance fading and needed to find new ways to<br/>
@@ -116,15 +116,15 @@ function Trainers() {
                                        she left her 9-to-5 to pursue a career in fitness. Melody's goal<br/>
                                        is to help people feel confident both in the studio and their<br/>
                                        day-to-day lives.</h1>
-                        <h1 className="text-white text-lg mt-6"><span className="font-bold">Fun Fact: </span>
+                        <h1 className="text-white md:text-lg text-xl mt-6"><span className="font-bold">Fun Fact: </span>
                              Mel would love to train Miley Cyrus one day. "I'm all<br/>
                              about releasing some serious girl power!"</h1>
-                             <h1 className="text-white text-lg mt-6 font-bold">Certifications:</h1>
-                            <h1 className="text-white mt-2 text-lg">NASM Personal Trainer, AFAA Group Fitness Instructor,<br/>
+                             <h1 className="text-white md:text-lg text-xl mt-6 font-bold">Certifications:</h1>
+                            <h1 className="text-white mt-2 md:text-lg text-xl">NASM Personal Trainer, AFAA Group Fitness Instructor,<br/>
                             PROnatal Fitness Pre and Post Natal, TRX Level 1</h1><button className=" btn2 mt-8  border  px-4 py-2 font-semibold">BOOK OLIVIA CLASS</button>
                     </div>
                     <div>
-                       <img data-aos="fade-up" className="" src={trainer4} style={{height:"500px",width:"540px"}}/>
+                       <img data-aos="fade-up" className="md:mt-0 mt-20 md:h-[500px] md:w-[540px] h-[320px] w-[300px] md:ml-0 ml-10" src={trainer4}/>
                     </div>
                 </div>
         </div>
